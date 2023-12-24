5588d129a8b3cf61d365a0a3254b47774362395d
pashi <pashireddi@gmail.com>
2023-12-24 01:25:48 -0600

    added  fucntion ality  with modified   pointer  referencing

--- structureRecursion.c
+++ structureRecursion.c
@@ -3,16 +3,16 @@
 #include<string.h>
 
 
-typedef struct Kake {
+typedef struct  kake {
     int age ;
     char* name ;
 
-    struct  Kake (*add)(struct Kake);
-      struct   Kake (*subtract)(struct Kake);
-}Kake;
+     struct  kake(*add)(struct kake);
+   struct kake (*subtract)(struct   kake);
+}lafoor;
 
 // Function definitions
-struct Kake add(struct Kake  m) {
+ lafoor add(lafoor  m) {
 
 m.age=  256;
 
@@ -20,7 +20,7 @@ return  m;
 
 }
 
-struct  Kake subtract(struct  Kake  jk) {
+  lafoor subtract(   lafoor  jk) {
 jk.name  = (char*)malloc(100 * sizeof(char));
 jk.name   = "pashi";
 
@@ -31,29 +31,44 @@ return jk;
 int main() {
 
 
-  Kake  one;
-Kake  object;
+//   kake  one;
+// kake  object;
 
 
-Kake receive;
+// kake receive;
 
-one.add  = add;
+// one.add  = add;
 
-one.subtract  = subtract;
+// one.subtract  = subtract;
 
- receive =  one.add(object);
-    printf("Age after addition: %d\n", receive.age);
+//  receive =  one.add(object);
+//     printf("Age after addition: %d\n", receive.age);
 
-receive  =  one.subtract(object);
-    printf("Name after subtraction: %s\n", receive.name);
+// receive  =  one.subtract(object);
+//     printf("Name after subtraction: %s\n", receive.name);
+
+// free(receive.name);
+
+lafoor  pointer  =  {0}; 
+lafoor  object;
+lafoor  reciever;
+pointer.add=  add;
+reciever  =pointer.add(object);
+printf( "the  age is %d\n", reciever.age);
+
+pointer.subtract =  subtract;
+
+reciever =  pointer.subtract(object);
+printf("the name is %s\n", reciever.name);
 
 
 
 
 
+free(object.name);
 
 
-free(receive.name);
+
 
     return 0;
 }
